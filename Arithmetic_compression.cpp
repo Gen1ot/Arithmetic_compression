057a2e389b6f5fd6ea213f089854fe21a047c673
Gen1ot <91340112+Gen1ot@users.noreply.github.com>
2023-01-07 00:17:03 +0700

    Update Arithmetic_compression.cpp

--- Arithmetic_compression.cpp
+++ Arithmetic_compression.cpp
@@ -1,1 +1,10 @@
+#include <sys/stat.h>
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
 
+// Number of bits in a code value
+#define code_value_bits 16
